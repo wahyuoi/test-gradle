f3b4cf60be6172907ff8093eeef73b62319c64de
Gede Wahyu <tokekbesi@gmail.com>
2019-10-04 09:52:54 +0700

    add gradle test

---
+++ src/test/java/com/tokekbesi/TestHello.java
@@ -0,0 +1,11 @@
+package com.tokekbesi;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestHello {
+  @Test
+  public void test_ok() {
+    Assert.assertEquals(100, 50 * 2);
+  }
+}
